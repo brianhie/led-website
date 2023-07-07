470abf6e711819d4923174e9b4a7cd0045b54cc6
Brian Hie <blhie1@gmail.com>
2023-07-07 08:48:26 +0100

    fix responsive header, remove html links

--- ledlab.js
+++ ledlab.js
@@ -1,21 +1,32 @@
-var hamburgerMenu = document.getElementById('hamburger-menu');
-var dropdown = document.getElementById('dropdown');
+function renderNav(html_fname) {
+    fetch(html_fname)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+            addEventListeners();
+        });
+}
 
-hamburgerMenu.addEventListener('click', function(event) {
-    event.stopPropagation();
-    if (dropdown.classList.contains('dropdown-show')) {
-        dropdown.classList.remove('dropdown-show');
-    } else {
-        dropdown.classList.add('dropdown-show');
-    }
-});
+function addEventListeners() {
+    var hamburgerMenu = document.getElementById('hamburger-menu');
+    var dropdown = document.getElementById('dropdown');
 
-dropdown.addEventListener('click', function(event) {
-    event.stopPropagation();
-});
+    hamburgerMenu.addEventListener('click', function(event) {
+        event.stopPropagation();
+        if (dropdown.classList.contains('dropdown-show')) {
+            dropdown.classList.remove('dropdown-show');
+        } else {
+            dropdown.classList.add('dropdown-show');
+        }
+    });
 
-document.body.addEventListener('click', function() {
-    if (dropdown.classList.contains('dropdown-show')) {
-        dropdown.classList.remove('dropdown-show');
-    }
-});
+    dropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+
+    document.body.addEventListener('click', function() {
+        if (dropdown.classList.contains('dropdown-show')) {
+            dropdown.classList.remove('dropdown-show');
+        }
+    });
+}
